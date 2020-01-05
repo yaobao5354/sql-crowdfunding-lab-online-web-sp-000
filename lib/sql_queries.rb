c45ce517fef334a50ed0b095121fc8016f622efe
yaoyu bao <yaobao5354@gmail.com>
2020-01-05 06:13:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,9 +42,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, pledges.amount
   FROM projects
   INNER JOIN pledges
-  ON users.id = pledges.user_id
-  WHERE projects.category = 'music'
-  ORDER BY projects.category"
+  "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category

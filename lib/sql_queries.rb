0ddaf9be374c86d31e9fc2d7b0757de18aead12b
yaoyu bao <yaobao5354@gmail.com>
2020-01-05 06:23:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -50,6 +50,7 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   "SELECT projects.category, pledges.amount
   FROM projects
   INNER JOIN pledges
+  ON project.id = pledges.project_id
   WHERE projects.category = 'books'
   GROUP BY projects.category"
 end
